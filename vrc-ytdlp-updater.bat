ba44ba722d1c096c8d84d2347472d13889f8d781
Kaiera <fia.kaiera@gmail.com>
2026-03-09 16:31:14 +0800

    Enhance yt-dlp updater with integrity level settings
    Updated the updater script to include setting the integrity level of yt-dlp.exe to Medium and the old one to Low.

--- vrc-ytdlp-updater.bat
+++ vrc-ytdlp-updater.bat
@@ -13,7 +13,8 @@ echo 	This file will do the following:
 echo 	1. Downloads the latest version of yt-dlp.exe from github.
 echo 	   (https://github.com/yt-dlp/yt-dlp)
 echo 	2. Renames the old yt-dlp.exe in the same folder.
-echo 	3. Sets the new yt-dlp.exe as read-only and removing read-only on the old one.
+echo 	3. Sets the new yt-dlp.exe as read-only, while the old one's read-only attribute is removed.
+echo 	4. Sets the new yt-dlp.exe's integrity level to Medium, while the old one is set to Low.
 echo.
 echo You can find your copy of yt-dlp and its backup here:
 echo %LOCALAPPDATA%low\VRChat\VRChat\Tools
@@ -22,6 +23,7 @@ echo If you want to remove this, close this window and run this by entering the 
 echo cmd /c attrib -r "%LOCALAPPDATA%Low\VRChat\VRChat\Tools\yt-dlp.exe"
 echo.
 echo █████ WARNING: This will close VRChat if you have it open.
+echo █████ If you have VRCX, it can re-open VRChat if it's set to do so.
 echo █████ If you don't want to do it right now, then simply close this window.
 echo.
 echo --------------------------------------------------------------------
@@ -34,6 +36,7 @@ echo ただし日本語への翻訳が必要になるかもしれません：
 echo https://github.com/fiaKaiera/vrc-ytdlp-updater
 echo.
 echo █████ ※ VRChatが開いている場合、これを閉じます。
+echo █████ VRCXをお持ちの場合、設定に応じてVRChatを再起動できます。
 echo █████ 今すぐ実行したくない場合は、このウィンドウを閉じるだけで結構です。
 echo --------------------------------------------------------------------
 echo.
@@ -71,8 +74,10 @@ if exist "%FILENAME_PRE%%n%.exe" (
     goto backuprenloop
 )
 set FILENAME=%FILENAME_PRE%%n%.exe
-echo Removing 'Read-only' attribute on 'yt-dlp.exe'
+echo Removing 'Read-only' attribute on old 'yt-dlp.exe'
 attrib -r yt-dlp.exe
+echo Setting integrity level of old 'yt-dlp.exe' to Low
+icacls yt-dlp.exe /setintegritylevel l
 ren yt-dlp.exe %FILENAME%
 echo Backup complete!
 echo Backup is located at:
@@ -88,10 +93,16 @@ echo Renaming downloaded 'yt-dlp.new.exe' to 'yt-dlp.exe'
 ren yt-dlp.new.exe yt-dlp.exe
 echo Adding 'Read-only' attribute to 'yt-dlp.exe'
 attrib +r yt-dlp.exe
+echo Setting integrity level of 'yt-dlp.exe' to Medium
+icacls yt-dlp.exe /setintegritylevel m
 :: ----------------------------------------------------
 
 echo.
 echo Process is complete! 処理が完了しました！
+
+tasklist /fi "ImageName eq VRCX.exe" /fo csv 2>NUL | find /I "VRCX.exe">NUL
+if "%ERRORLEVEL%"=="0" echo VRCX is running, so if you have VRChat open before-hand, you should see it re-open shortly.
+
 :exit
 echo Press any key to close... 任意のキーを押して終了してください...
 pause>nul
